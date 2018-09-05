3bec91a817d527933b594caa7776b2117f78fd33
guigeng <guigeng.ding@gmail.com>
2018-09-05 14:20:41 +0800

    test

--- squid/entrypoint.sh
+++ squid/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "
+cat > /etc/squid/peer.conf <<EOF
 cache_peer ${PEER_SERVER} parent ${PEER_PORT} 0 no-digest proxy-only
 prefer_direct off
 nonhierarchical_direct off
-acl chinaip dst '/etc/squid/cnip.txt'
+acl chinaip dst "/etc/squid/cnip.txt"
 always_direct allow chinaip
 never_direct allow all
-" > /etc/squid/peer.conf
+EOF
 
 exec /usr/sbin/squid -f ${SQUID_CONFIG_FILE} --foreground -NYCd 1
